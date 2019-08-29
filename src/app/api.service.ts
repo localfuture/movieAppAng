dfa1b50ba5d4e5be41c932cd3953a88b8b25f245
Anand Alex <anandalex1993@gmail.com>
2019-08-29 13:02:54 +0530

    service address changed

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,16 +9,16 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   insertData(data){
-   return this.http.post("http://localhost:3000/addMovie",data);
+   return this.http.post("https://api-movie-angular.herokuapp.com/addMovie",data);
    
   }
 
   viewData(){
-    return this.http.get("http://localhost:3000/viewMovie");
+    return this.http.get("https://api-movie-angular.herokuapp.com/viewMovie");
   }
 
   searchData(data){
-    return this.http.post("http://localhost:3000/searchMovie",data);
+    return this.http.post("https://api-movie-angular.herokuapp.com/searchMovie",data);
   }
 
 }
